283240f194f97af7e676f38636a9373419964841
himanshu <himanshurajoriya9510@gmail.com>
2025-08-15 16:51:24 +0530

    add link

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
     app.config.update({
     #    'SQLALCHEMY_DATABASE_URI': "postgresql://flaskdb_60cb_user:5YN8MXljQ395a0gqS5jSTmIb7lcsRLY2@dpg-d2fevlumcj7s73eogqdg-a.oregon-postgres.render.com/flaskdb_60cb",
-        "SQLALCHEMY_DATABASE_URI": "postgresql://postgres:himanshu%409510@localhost:5432/flaskdb",
+        "SQLALCHEMY_DATABASE_URI": "postgresql://expanse_tracker_mivg_user:5FHPEC09UNcNok3hw3dCpuCxtGwblviU@dpg-d2fh63idbo4c73b9d4bg-a.oregon-postgres.render.com/expanse_tracker_mivg",
         "SQLALCHEMY_TRACK_MODIFICATIONS": False,
         "SQLALCHEMY_ECHO": True,
     })
